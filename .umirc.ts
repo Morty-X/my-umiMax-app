0742bd74c09e02e569238a5c78cee8a50bba9ca7
Morty <3089326386@qq.com>
2025-04-28 14:28:07 +0800

    feat: init

---
+++ .umirc.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+  // antd配置
+  antd: {},
+
+  //权限  access 插件依赖 initial State 所以需要同时开启
+  access: {},
+
+  model: {},
+  initialState: {},
+  request: {},
+  layout: {
+    title: '@umijs/max',
+  },
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      name: '首页',
+      path: '/home',
+      component: './Home',
+    },
+    {
+      name: '权限演示',
+      path: '/access',
+      component: './Access',
+    },
+    {
+      name: ' CRUD 示例',
+      path: '/table',
+      component: './Table',
+    },
+  ],
+  npmClient: 'pnpm',
+});
